ffcb6dbd9ffbef6301e7d2881919c98d0be658f0
Amund Tenstad <github@amund.io>
2022-12-13 11:03:26 +0100

    Implement generic resource types (#13)
    * Implement generic event publisher
    
    * debug log rabbitmq messages
    
    * fix: read harvest graphs from harvester
    
    * fix: HARVESTER_API_URL
    
    * fix: dont stop on first error in report
    
    * Add routing_key to debug log
    
    * Fix import
    
    Co-authored-by: Nils Ove Tendenes <50194012+NilsOveTen@users.noreply.github.com>
    
    Co-authored-by: Nils Ove Tendenes <50194012+NilsOveTen@users.noreply.github.com>

---
+++ src/bin/fdk-dataset-event-publisher.rs
@@ -0,0 +1,141 @@
+use std::env;
+
+use async_trait::async_trait;
+use lazy_static::lazy_static;
+use serde_derive::Serialize;
+
+use fdk_event_publisher::{
+    error::Error, kafka, run_event_publisher, utils::http_get, ChangeType, EventConfig, Resource,
+    ResourceConfig,
+};
+
+lazy_static! {
+    static ref HARVESTER_API_URL: String =
+        env::var("HARVESTER_API_URL").unwrap_or("http://localhost:8081".to_string());
+    static ref REASONING_API_URL: String =
+        env::var("REASONING_API_URL").unwrap_or("http://localhost:8082".to_string());
+    static ref CONSUMER_NAME: String =
+        env::var("CONSUMER_NAME").unwrap_or("fdk-dataset-event-publisher".to_string());
+    static ref OUTPUT_TOPIC: String =
+        env::var("OUTPUT_TOPIC").unwrap_or("dataset-events".to_string());
+}
+
+#[tokio::main]
+async fn main() {
+    let resource_config = ResourceConfig {
+        consumer_name: CONSUMER_NAME.clone(),
+        routing_keys: vec![
+            "datasets.harvested".to_string(),
+            "datasets.reasoned".to_string(),
+        ],
+    };
+
+    let event_config = EventConfig {
+        name: "no.fdk.dataset.DatasetEvent".to_string(),
+        topic: OUTPUT_TOPIC.clone(),
+        schema: r#"{
+                "name": "DatasetEvent",
+                "namespace": "no.fdk.dataset",
+                "type": "record",
+                "fields": [
+                    {
+                        "name": "type",
+                        "type": {
+                            "type": "enum",
+                            "name": "DatasetEventType",
+                            "symbols": ["DATASET_HARVESTED", "DATASET_REASONED", "DATASET_REMOVED"]
+                        }
+                    },
+                    {"name": "fdkId", "type": "string"},
+                    {"name": "graph", "type": "string"},
+                    {"name": "timestamp", "type": "long", "logicalType": "timestamp-millis"}
+                ]
+            }"#
+        .to_string(),
+    };
+
+    tracing_subscriber::fmt()
+        .json()
+        .with_max_level(tracing::Level::DEBUG)
+        .with_target(false)
+        .with_current_span(false)
+        .init();
+
+    run_event_publisher::<Dataset>(resource_config, event_config).await
+}
+
+pub struct Dataset {}
+
+#[async_trait]
+impl Resource for Dataset {
+    type Event = DatasetEvent;
+
+    async fn event(
+        routing_key: &str,
+        id: String,
+        timestamp: i64,
+        report_change: ChangeType,
+    ) -> Result<Option<Self::Event>, Error> {
+        let event_type = match report_change {
+            ChangeType::CreateOrUpdate => DatasetEventType::from_routing_key(routing_key),
+            ChangeType::Remove => Ok(DatasetEventType::DatasetRemoved),
+        }?;
+
+        let graph = match event_type {
+            DatasetEventType::DatasetHarvested => {
+                http_get(format!("{}/datasets/{}", HARVESTER_API_URL.as_str(), id)).await
+            }
+            DatasetEventType::DatasetReasoned => {
+                http_get(format!("{}/datasets/{}", REASONING_API_URL.as_str(), id)).await
+            }
+            // Do not bother fetching graph for remove events
+            DatasetEventType::DatasetRemoved => Ok("".to_string()),
+        }?;
+
+        Ok(Some(Self::Event {
+            event_type,
+            fdk_id: id,
+            graph,
+            timestamp,
+        }))
+    }
+}
+
+#[derive(Debug, Serialize)]
+pub struct DatasetEvent {
+    #[serde(rename = "type")]
+    pub event_type: DatasetEventType,
+    #[serde(rename = "fdkId")]
+    pub fdk_id: String,
+    pub graph: String,
+    pub timestamp: i64,
+}
+
+impl kafka::Event for DatasetEvent {
+    fn key(&self) -> String {
+        self.fdk_id.clone()
+    }
+}
+
+#[derive(Clone, Copy, Debug, Serialize)]
+pub enum DatasetEventType {
+    #[serde(rename = "DATASET_HARVESTED")]
+    DatasetHarvested,
+    #[serde(rename = "DATASET_REASONED")]
+    DatasetReasoned,
+    #[serde(rename = "DATASET_REMOVED")]
+    DatasetRemoved,
+}
+
+impl DatasetEventType {
+    fn from_routing_key(routing_key: &str) -> Result<Self, Error> {
+        match routing_key {
+            "datasets.harvested" => Ok(Self::DatasetHarvested),
+            "datasets.reasoned" => Ok(Self::DatasetReasoned),
+            _ => Err(Error::String(format!(
+                "unknown routing key: '{}'",
+                routing_key
+            ))),
+        }
+    }
+}
---
+++ src/lib.rs
@@ -0,0 +1,272 @@
+use std::time::Instant;
+
+use async_trait::async_trait;
+use chrono::DateTime;
+use error::Error;
+use kafka::create_sr_settings;
+use lapin::{
+    message::{Delivery, DeliveryResult},
+    options::BasicAckOptions,
+};
+use lazy_static::lazy_static;
+use rabbit::HarvestReport;
+use rdkafka::producer::FutureProducer;
+use schema_registry_converter::async_impl::{avro::AvroEncoder, schema_registry::SrSettings};
+
+use crate::{
+    http::run_http_server,
+    kafka::{send_event, BROKERS, SCHEMA_REGISTRY},
+    metrics::{register_metrics, PROCESSED_MESSAGES, PROCESSING_TIME},
+    schema::setup_schema,
+};
+
+pub mod error;
+mod http;
+pub mod kafka;
+mod metrics;
+mod rabbit;
+mod schema;
+pub mod utils;
+
+lazy_static! {
+    pub static ref PRODUCER: FutureProducer = kafka::create_producer().unwrap_or_else(|e| {
+        tracing::error!(error = e.to_string(), "kafka producer creation error");
+        std::process::exit(1);
+    });
+    pub static ref SR_SETTINGS: SrSettings = create_sr_settings().unwrap_or_else(|e| {
+        tracing::error!(error = e.to_string(), "sr settings creation error");
+        std::process::exit(1);
+    });
+}
+
+pub struct ResourceConfig {
+    pub consumer_name: String,
+    pub routing_keys: Vec<String>,
+}
+
+#[derive(Clone)]
+pub struct EventConfig {
+    pub name: String,
+    pub topic: String,
+    pub schema: String,
+}
+
+#[async_trait]
+pub trait Resource {
+    type Event: kafka::Event + Send;
+
+    async fn event(
+        routing_key: &str,
+        id: String,
+        timestamp: i64,
+        change: ChangeType,
+    ) -> Result<Option<Self::Event>, Error>;
+}
+
+#[derive(Debug)]
+pub enum ChangeType {
+    CreateOrUpdate,
+    Remove,
+}
+
+pub async fn run_event_publisher<R: Resource + 'static>(
+    resource_config: ResourceConfig,
+    event_config: EventConfig,
+) {
+    tracing::info!(
+        brokers = BROKERS.to_string(),
+        schema_registry = SCHEMA_REGISTRY.to_string(),
+        consumer_name = resource_config.consumer_name,
+        output_topic = event_config.topic,
+        routing_keys = format!("{:?}", resource_config.routing_keys),
+        "starting service"
+    );
+
+    register_metrics();
+
+    setup_schema(&SR_SETTINGS, &event_config)
+        .await
+        .unwrap_or_else(|e| {
+            tracing::error!(error = e.to_string(), "schema registration error");
+            std::process::exit(1);
+        });
+
+    let channel = rabbit::connect().await.unwrap_or_else(|e| {
+        tracing::error!(error = e.to_string(), "rabbit connection error");
+        std::process::exit(1);
+    });
+    rabbit::setup(
+        &channel,
+        &resource_config.consumer_name,
+        &resource_config.routing_keys,
+    )
+    .await
+    .unwrap_or_else(|e| {
+        tracing::error!(error = e.to_string(), "rabbit setup error");
+        std::process::exit(1);
+    });
+    let consumer = rabbit::create_consumer(&channel, &resource_config.consumer_name)
+        .await
+        .unwrap_or_else(|e| {
+            tracing::error!(error = e.to_string(), "rabbit consumer creation error");
+            std::process::exit(1);
+        });
+
+    consumer.set_delegate(move |delivery| receive_message::<R>(event_config.clone(), delivery));
+
+    run_http_server().await.unwrap_or_else(|e| {
+        tracing::error!(error = e.to_string(), "failed to run http server");
+        std::process::exit(1);
+    });
+}
+
+async fn receive_message<R: Resource>(event_config: EventConfig, delivery: DeliveryResult) {
+    let delivery = match delivery {
+        Ok(Some(delivery)) => delivery,
+        Ok(None) => return,
+        Err(error) => {
+            tracing::error!(error = error.to_string(), "failed to consume message");
+            std::process::exit(1);
+        }
+    };
+
+    let start_time = Instant::now();
+    let result =
+        handle_message::<R>(&PRODUCER, SR_SETTINGS.clone(), &event_config, &delivery).await;
+    let elapsed_millis = start_time.elapsed().as_millis();
+
+    let metric_status_label = match result {
+        Ok(_) => {
+            tracing::info!(elapsed_millis, "message handled successfully");
+            "success"
+        }
+        Err(e) => {
+            tracing::error!(
+                elapsed_millis,
+                error = e.to_string(),
+                "failed while handling message"
+            );
+            "error"
+        }
+    };
+    PROCESSED_MESSAGES
+        .with_label_values(&[metric_status_label])
+        .inc();
+    PROCESSING_TIME.observe(elapsed_millis as f64 / 1000.0);
+
+    delivery
+        .ack(BasicAckOptions::default())
+        .await
+        .unwrap_or_else(|e| tracing::error!(error = e.to_string(), "failed to ack message"));
+}
+
+async fn handle_message<R: Resource>(
+    producer: &FutureProducer,
+    sr_settings: SrSettings,
+    event_config: &EventConfig,
+    delivery: &Delivery,
+) -> Result<(), Error> {
+    let reports: Vec<HarvestReport> = serde_json::from_slice(&delivery.data)?;
+
+    let changed_resource_count = reports
+        .iter()
+        .map(|element| element.changed_resources.len())
+        .sum::<usize>();
+    let removed_resource_count = reports
+        .iter()
+        .map(|element| {
+            element
+                .removed_resources
+                .as_ref()
+                .map_or(0, |resources| resources.len())
+        })
+        .sum::<usize>();
+
+    tracing::debug!(
+        routing_key = delivery.routing_key.as_str(),
+        reports = format!("{:?}", reports),
+        "processing event"
+    );
+
+    tracing::info!(
+        routing_key = delivery.routing_key.as_str(),
+        reports = reports.len(),
+        changed_resource_count,
+        removed_resource_count,
+        "processing event"
+    );
+    let mut encoder = AvroEncoder::new(sr_settings);
+
+    for element in reports {
+        let timestamp = DateTime::parse_from_str(&element.start_time, "%Y-%m-%d %H:%M:%S%.f %z")?
+            .timestamp_millis();
+
+        for resource in element.changed_resources {
+            if let Err(e) = handle_event::<R>(
+                &mut encoder,
+                &producer,
+                &event_config,
+                delivery.routing_key.as_str(),
+                resource.fdk_id.clone(),
+                timestamp,
+                ChangeType::CreateOrUpdate,
+            )
+            .await
+            {
+                tracing::error!(
+                    id = resource.fdk_id,
+                    change = format!("{:?}", ChangeType::CreateOrUpdate),
+                    error = e.to_string(),
+                    "failed while handling event"
+                );
+            }
+        }
+
+        if let Some(removed_resources) = element.removed_resources {
+            for resource in removed_resources {
+                if let Err(e) = handle_event::<R>(
+                    &mut encoder,
+                    &producer,
+                    &event_config,
+                    delivery.routing_key.as_str(),
+                    resource.fdk_id.clone(),
+                    timestamp,
+                    ChangeType::Remove,
+                )
+                .await
+                {
+                    tracing::error!(
+                        id = resource.fdk_id,
+                        change = format!("{:?}", ChangeType::Remove),
+                        error = e.to_string(),
+                        "failed while handling event"
+                    );
+                }
+            }
+        }
+    }
+
+    Ok(())
+}
+
+async fn handle_event<R: Resource>(
+    mut encoder: &mut AvroEncoder<'_>,
+    producer: &FutureProducer,
+    event_config: &EventConfig,
+    routing_key: &str,
+    id: String,
+    timestamp: i64,
+    change: ChangeType,
+) -> Result<(), Error> {
+    tracing::debug!(
+        routing_key,
+        id = id.as_str(),
+        change = format!("{:?}", change),
+        "processing event"
+    );
+
+    if let Some(event) = R::event(routing_key, id, timestamp, change).await? {
+        send_event(&mut encoder, &producer, &event_config, event).await?;
+    };
+    Ok(())
+}
